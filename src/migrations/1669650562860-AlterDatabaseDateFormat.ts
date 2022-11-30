90a1aea48c8a1c057f54bf1bdfb67607c5f3c219
Paloma Araujo <paclaraujo@gmail.com>
2022-11-30 16:21:26 -0300

    fix: migration sql query

--- src/migrations/1669650562860-AlterDatabaseDateFormat.ts
+++ src/migrations/1669650562860-AlterDatabaseDateFormat.ts
@@ -3,14 +3,10 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class AlterDatabaseDateFormat1669650562860
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER DATABASE kpis_api SET datestyle TO 'ISO, DMY'`
-    );
+    await queryRunner.query(`SET datestyle TO 'ISO, DMY'`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER DATABASE kpis_api SET datestyle TO 'ISO, YMD'`
-    );
+    await queryRunner.query(`SET datestyle TO 'ISO, YMD'`);
   }
 }
